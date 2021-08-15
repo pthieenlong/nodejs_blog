5f5e35d9e7c1bda7257f635f7c01522ce8fb8602
pthieenlong <p.thieenlong.304@gmail.com>
2021-08-15 16:46:19 +0700

    rechanges

--- src/index.js
+++ src/index.js
@@ -5,14 +5,28 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+//handlebar
 app.engine('hbs', expressHandlebar({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname));
+app.set('views', path.join(__dirname, 'resources'));
+// Morgan
 app.use(morgan('combined'));
+//express
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(path.join(__dirname, 'public'));
+//engine
 app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/item', (req, res) => {
   res.render('item')
+})
+app.get('/test', (req, res) => {
+  res.render('test')
 })
 
 
