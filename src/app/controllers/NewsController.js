aa2b0a4db4f21f764eb509e6da417bca3b1005d9
pthieenlong <p.thieenlong.304@gmail.com>
2021-08-16 10:10:59 +0700

    test lint-staged and edit pages

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
+
     //[GET] /news
     index(req, res) {
         res.render('news');
     }
 
     //[GET] /news/:slug
-
-    show(req, res) {
-        res.send('new details');
+          show(req, res) {
+          res.send('new details');
     }
 }
 
