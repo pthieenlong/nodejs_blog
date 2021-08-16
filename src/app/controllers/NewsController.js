843cd0c42ef9d3976d62bef27c6dcb4a6dd55cc3
pthieenlong <p.thieenlong.304@gmail.com>
2021-08-16 10:42:08 +0700

    test husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,11 @@
 class NewsController {
-
     //[GET] /news
     index(req, res) {
         res.render('news');
     }
     //[GET] /news/:slug
-         show(req, res) {
-       res.send('new details');
+    show(req, res) {
+        res.send('new details');
     }
 }
 
